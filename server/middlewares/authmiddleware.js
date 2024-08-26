03c83fa336225d9159e191626983654bf072b450
RupaSravanthi <158283169+RupaSravanthi@users.noreply.github.com>
2024-08-26 16:12:12 +0100

    adding middleware

---
+++ server/middlewares/authmiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'; // Assuming you're using JWT for token generation
+
+// Middleware to authenticate and get user from token
+export const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.status(401).json({ message: 'No token provided.' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token.' });
+        req.user = user;
+        next();
+    });
+};
